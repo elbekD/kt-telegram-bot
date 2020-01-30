07b296b35d11f879ffcb6fc1eeed0afce465dc34
dragneelfps <s.rawat3.142@live.in>
2020-01-30 22:38:03 +0530

    Added the fields total_voter_count, is_anonymous, type, allows_multiple_answers, correct_option_id to the Poll object.

--- library/src/main/kotlin/com/elbekD/bot/types/poll.kt
+++ library/src/main/kotlin/com/elbekD/bot/types/poll.kt
@@ -3,7 +3,12 @@ package com.elbekD.bot.types
 data class Poll(val id: String,
                 val question: String,
                 val options: List<PollOption>,
-                val is_closed: Boolean)
+                val total_voter_count: Int,
+                val is_closed: Boolean,
+                val is_anonymous: Boolean,
+                val type: String,
+                val allows_multiple_answers: Boolean,
+                val correct_option_id: Int?)
 
 data class PollOption(val text: String, val voter_count: Int)
 
